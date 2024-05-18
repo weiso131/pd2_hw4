68bdc4f4b67142df189a55adafa85a38551d3cc0
許育瑋 <blackfire106.1231@gmail.com>
2024-05-18 13:41:23 +0800

    2.3sec

--- TFIDFCalculator.java
+++ TFIDFCalculator.java
@@ -9,10 +9,13 @@ public class TFIDFCalculator {
 
     public static ArrayList<Trie> docSet = new ArrayList<Trie>();
     public static Trie document = new Trie();
-
+    public static long startTime = System.currentTimeMillis();
     public static void main(String[] args) {
         docSet = getDocString(args[0]);
+        System.out.println(String.format("read time: %d", System.currentTimeMillis() - startTime));
+        startTime = System.currentTimeMillis();
         output(args[1]);
+        System.out.println(String.format("write time: %d", System.currentTimeMillis() - startTime));
     }
 
     public static ArrayList<String> readLine(String fileName) {
@@ -30,9 +33,13 @@ public class TFIDFCalculator {
 
     public static ArrayList<Trie> getDocString(String fileName) {
         ArrayList<String> lines = readLine(fileName);
+
+        
         for (int i = 0; i < lines.size(); i++) {
+            
             String newLine = lines.get(i).toLowerCase().replaceAll("[^a-z]", " ");
             String[] words = newLine.split(" ");
+
 
             if ((i / 5) + 1 > docSet.size())
                 docSet.add(new Trie());
@@ -40,7 +47,12 @@ public class TFIDFCalculator {
             for (String word : words) {
                 if (word.length() == 0)
                     continue;
-                docSet.get(i / 5).insert(word, 1);
+
+                if (docSet.get(i / 5).search(word) == 0){
+                    document.insert(word);
+                }
+
+                docSet.get(i / 5).insert(word);
             }
         }
         return docSet;
@@ -53,31 +65,26 @@ public class TFIDFCalculator {
         String[] line1 = lines.get(0).split(" ");
         String[] line2 = lines.get(1).split(" ");
 
-        String output = "";
+        StringBuffer output_sb = new StringBuffer();
+        
+
         for (int i = 0; i < line1.length; i++) {
             String word = line1[i];
             int textNum = Integer.parseInt(line2[i]);
             double TFIDF = docSet.get(textNum).search(word) / docSet.get(textNum).totalWords;
-
+            
             if (TFIDF != 0) {
-                double tDcount = document.search(word);
-                if (tDcount == 0) {
-                    for (int j = 0; j < docSet.size(); j++) {
 
-                        double frequency = docSet.get(j).search(word);
-                        tDcount += (frequency != 0) ? 1 : 0;// can re use
-                    }
-                    document.insert(word, (int) tDcount);
-                }
+                double tDcount = document.search(word);
                 TFIDF *= Math.log((double) docSet.size() / tDcount);
             }
 
-            output += String.format("%.5f ", TFIDF);
+            output_sb.append(String.format("%.5f ", TFIDF));
 
         }
 
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"))) {
-            bw.write(output);
+            bw.write(output_sb.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -95,7 +102,7 @@ class Trie {
     TrieNode root = new TrieNode();
     double totalWords = 0;
 
-    public void insert(String word, int count) {
+    public void insert(String word) {
         TrieNode node = root;
         for (char c : word.toCharArray()) {
             if (node.children[c - 'a'] == null) {
@@ -103,18 +110,19 @@ class Trie {
             }
             node = node.children[c - 'a'];
         }
-        node.Frequency += count;
+        node.Frequency += 1;
         totalWords++;
     }
 
     public double search(String word) {
         TrieNode node = root;
+        
         for (char c : word.toCharArray()) {
             node = node.children[c - 'a'];
             if (node == null) {
                 return 0;
             }
         }
-        return (double) node.Frequency;
+        return node.Frequency;
     }
 }
